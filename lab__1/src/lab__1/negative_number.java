9213270681d958d8db603c90b36e91078114594b
lydia <lydia@10.0.2.15>
2021-10-17 13:00:42 -0700

    0

---
+++ lab__1/src/lab__1/negative_number.java
@@ -0,0 +1,14 @@
+package lab__1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class negative_number {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
