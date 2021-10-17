b1109092018c04a843591c8740ee68b668b310ad
lydia <lydia@10.0.2.15>
2021-10-17 13:14:42 -0700

    0

---
+++ lab__1/src/lab__1/euroToSterlingTest.java
@@ -0,0 +1,14 @@
+package lab__1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class euroToSterlingTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
